44ff55ef61f916be3b0c75ff03f3904c690ec2d4
Nur Wahida <wahidan23h@student.unhas.ac.id>
2026-06-24 23:29:02 +0800

    feat: hubungkan aksi check-in & check-out admin ke backend endpoint checkin/checkout

--- frontend-react/src/services/reservationService.js
+++ frontend-react/src/services/reservationService.js
@@ -32,6 +32,15 @@ export const getAllReservations = async () => {
 
 // Admin: Update status of a reservation
 export const updateReservationStatus = async (id, status) => {
-    const res = await api.put(`/reservasi/status/${id}`, { status_reservasi: status }, authHeader());
-    return res.data;
+    if (status === "Check In") {
+        const res = await api.post("/checkin", { id_reservasi: id }, authHeader());
+        return res.data;
+    } else if (status === "Selesai") {
+        const res = await api.post("/checkout", { id_reservasi: id }, authHeader());
+        return res.data;
+    } else {
+        const res = await api.put(`/reservasi/status/${id}`, { status_reservasi: status }, authHeader());
+        return res.data;
+    }
 };
+
